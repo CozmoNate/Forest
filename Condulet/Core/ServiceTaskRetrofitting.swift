b28a3d81c3e4c0e6351eb965b3f8d6e4a040395c
Natan Zalkin <natan.zalkin@me.com>
2018-11-04 07:03:17 +0200

    Add some retrofitter usage explanation. See descriptions of the methods of ServiceTaskRetrofitting protocol

--- Condulet/Core/ServiceTaskRetrofitting.swift
+++ Condulet/Core/ServiceTaskRetrofitting.swift
@@ -12,16 +12,21 @@ import Foundation
 /// Retrofitter class allows to modify default behaviour of ServiceTask without subclassing
 public protocol ServiceTaskRetrofitting {
 
-    /// Modify request before assigning it to URLSessionTask. Throwing error will cause the task to fail with provided error
+    /// Modify request before assigning it to URLSessionTask. Throw error to fail task and pass error to error handler and invoke error interception method.
     /// Return true to indicate that request is intercepted and no further handling should be peformed by ServiceTask when returned from this method.
+    /// When the request being intercepted use sendRequest(_:) method of the task to perform modified request.
+    /// When the request being intercepted use handleError(_:_:) method of the task to pass error to error handler.
     func serviceTask(_ task: ServiceTask, intercept request: inout URLRequest, action: ServiceTaskAction) throws -> Bool
 
     /// Intercept response handler. Throw error to fail task and pass error to error handler and invoke error interception method.
     /// Return true to indicate that response is intercepted and no further handling should be peformed by ServiceTask when returned from this method.
+    /// When the content being intercepted use handleContent(_:, _:) method of the task to pass content to content handler.
+    /// When the content being intercepted use handleError(_:_:) method of the task to pass error to error handler.
     func serviceTask(_ task: ServiceTask, intercept content: ServiceTaskContent, response: URLResponse) throws -> Bool
 
-    /// Intercept error handler. Throwing error will fail the task and pass error to error handler. You can use "throw" to map errors
+    /// Intercept error handler. Throwing error will fail the task and pass error directly to error handler.
     /// Return true to indicate that error is intercepted and no further handling should be peformed by ServiceTask when returned from this method.
+    /// When the error being intercepted use handleError(_:_:) method of the task to pass error to error handler.
     func serviceTask(_ task: ServiceTask, intercept error: Error, response: URLResponse?) throws -> Bool
 
 }
