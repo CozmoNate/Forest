8bf8d2bbaa5170b965c57a6267778667e0a7e7e6
Natan Zalkin <natan.zalkin@me.com>
2019-07-26 20:00:48 +0300

    Add support for Swift Package Manager. Add back “build” number to version number.

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Condulet",
+    platforms: [ .iOS(.v10) ],
+    products: [
+        .library(name: "ConduletCore", targets: ["ConduletCore"]),
+        .library(name: "ConduletReachability", targets: ["ConduletReachability"]),
+        .library(name: "ConduletProto", targets: ["ConduletProto"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
+    ],
+    targets: [
+        .target(name: "ConduletCore", dependencies: [], path: "Condulet/Core"),
+        .target(name: "ConduletReachability", dependencies: ["ConduletCore"], path: "Condulet/Reachability"),
+        .target(name: "ConduletProto", dependencies: ["ConduletCore", "SwiftProtobuf"], path: "Condulet/Protobuf"),
+    ],
+    swiftLanguageVersions: [.v5]
+)
